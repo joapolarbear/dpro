98811de5f98165106dace95eda921ee2e7338401
Hanpeng Hu <joapolarbear@gmail.com>
2020-12-16 18:14:41 +0800

    debug

--- setup.py
+++ setup.py
@@ -39,7 +39,8 @@ setup(name='byteprofile analysis',
           'matplotlib',
           'pymc3',
           'tensorflow',
-          'tqdm'
+          'tqdm',
+          "cvxpy"
       ],
       # zip_safe=False
       )
