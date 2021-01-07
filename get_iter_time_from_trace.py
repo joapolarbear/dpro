a0c7093e70be6159b8567a314e14d47822abda5b
chenyu-jiang <1155092088@link.cuhk.edu.hk>
2021-01-07 21:45:11 +0800

    modified utility tool

--- get_iter_time_from_trace.py
+++ get_iter_time_from_trace.py
@@ -1,30 +1,50 @@
 import json
 
-with open("/Users/chenyu/temp_1_NONE.json", "r") as f:
+with open("/Users/chenyu/temp_1_FW_UPD.json", "r") as f:
     trace = json.load(f)
 
 one_pid = -1
 for ev in trace["traceEvents"]:
-    if ev["ph"] == "M":
-        if ev["name"] == "process_name" and "name" in ev["args"]:
-            if "/job:localhost/replica:0/task:0/device:GPU:" in ev["args"]["name"] \
-                and "Compute" in ev["args"]["name"]:
-                one_pid = ev["pid"]
+    # if ev["ph"] == "M":
+    #     if ev["name"] == "process_name" and "name" in ev["args"]:
+    #         # if "/job:localhost/replica:0/task:0/device:GPU:" in ev["args"]["name"] \
+    #         print(ev["args"]["name"])
+    #         if "stream:all" in ev["args"]["name"] \
+    #             and "Compute" in ev["args"]["name"]:
+    #             one_pid = ev["pid"]
+    one_pid = ev["pid"]
+    break
 
 assert one_pid != -1
 print("one_pid: {}".format(one_pid))
 
+evs = sorted(trace["traceEvents"], key=lambda x: x["ts"])
+
 source_sts = []
 dep_eds = []
-for ev in trace["traceEvents"]:
+started = False
+last_assign = -1
+for ev in evs:
     if ev["ph"] == "X" and ev["pid"] == one_pid:
-        if "args" in ev and ev["args"]["name"] == "_SOURCE":
+        # if "args" in ev and ev["args"]["name"] == "_SOURCE":
+        if "args" in ev and "RandomUniform" in ev["args"]["name"] and started == False:
             source_sts.append(ev["ts"])
-        elif "args" in ev and ev["args"]["name"] == "group_deps_1":
-            dep_eds.append(ev["ts"] + ev["dur"])
+            if last_assign != -1:
+                dep_eds.append(last_assign)
+                last_assign = -1
+            started = True
+        # elif "args" in ev and ev["args"]["name"] == "group_deps_1":
+        elif "args" in ev and "Assign" in ev["args"]["name"]:
+            started = False
+            last_assign = ev["ts"] + ev["dur"]
+if last_assign != -1:
+    dep_eds.append(last_assign)
 
 source_sts = sorted(source_sts)
 dep_eds = sorted(dep_eds)
+
+# import code
+# code.interact(local=locals())
 
 iter_times = []
 for i in range(len(source_sts)):
